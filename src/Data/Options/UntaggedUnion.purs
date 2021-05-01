ac9cd608ad80f2a79c87f730a3ccd1a427e9235d
PureFunctor <purefunctor@gmail.com>
2021-05-01 20:45:55 +0800

    Hoist untaggedToOptions as a top-level binding

--- src/Data/Options/UntaggedUnion.purs
+++ src/Data/Options/UntaggedUnion.purs
@@ -1,13 +1,14 @@
 -- | Utilities for `untagged-union`.
 module Data.Options.UntaggedUnion
   ( optU
+  , untaggedToOptions
   )
   where
 
 import Prelude
 
 import Data.Op (Op(..))
-import Data.Options (Option, defaultToOptions)
+import Data.Options (Option, Options, defaultToOptions)
 import Type.Proxy (Proxy)
 import Untagged.Union (class InOneOf, OneOf, asOneOf)
 
@@ -23,6 +24,19 @@ optU
   => Proxy (OneOf h t)
   -> String
   -> Option o v
-optU _ = Op <<< untaggedToOptions
-  where
-    untaggedToOptions k v = defaultToOptions k (asOneOf v :: OneOf h t)
+optU p = Op <<< untaggedToOptions p
+
+
+-- | Builds an `Options o` from a `v` that can be coerced
+-- | using `InOneOf`.
+-- |
+-- | Similar to `optU`, the `h` and `t` type parameters
+-- | are provided through a proxied `OneOf` type.
+untaggedToOptions
+  :: forall o v h t
+   . InOneOf v h t
+  => Proxy (OneOf h t)
+  -> String
+  -> v
+  -> Options o
+untaggedToOptions _ k v = defaultToOptions k (asOneOf v :: OneOf h t)
